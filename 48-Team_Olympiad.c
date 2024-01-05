bdeef4c64327c1bcc92c3a2992cf975e5ba35516
Mostafa Asaad <ma4190243@gmail.com>
2024-01-06 00:46:42 +0200

    Problem 48 is modefied

--- 48-Team_Olympiad.c
+++ 48-Team_Olympiad.c
@@ -6,7 +6,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int check_if_zero(int *array){
+int check_for_zeros(int *array)
+{
     for(int i = 0; i < 3; ++i)
         if(array[i] == 0)
             return 1;
@@ -33,19 +34,77 @@ void ASC_sort(int *array, int size)
     }
 }
 
+void print_array(int *array, int size)
+{
+    for(int f_counter = 0; f_counter < size - 1; ++f_counter)
+        printf("%d ", array[f_counter]);
+
+    printf("%d\n", array[size-1]);    
+}
+
+void solve(int *skills, int number_of_students, int max_teams)
+{
+    int indexes[max_teams][3];
+    for(int i = 0; i < max_teams; i++)
+        for(int j = 0; j < 3; j++)
+            indexes[i][j] = 0;
+
+    for(int teams_counter = 0; teams_counter < max_teams; ++teams_counter)
+    {
+        for(int skills_counter = 0; skills_counter < number_of_students; ++skills_counter)
+        {
+            if (skills[skills_counter] == 1)
+            {
+                if(indexes[teams_counter][0] == 0)
+                {
+                    indexes[teams_counter][0] = skills_counter + 1;
+                    skills[skills_counter] = 0;
+                }
+            }
+
+            else if (skills[skills_counter] == 2)
+            {
+                if(indexes[teams_counter][1] == 0)
+                {
+                    indexes[teams_counter][1] = skills_counter + 1;
+                    skills[skills_counter] = 0;
+                }
+            }
+            
+            else if (skills[skills_counter] == 3)
+            {
+                if(indexes[teams_counter][2] == 0)
+                {
+                    indexes[teams_counter][2] = skills_counter + 1;
+                    skills[skills_counter] = 0;
+                }
+            }
+            /* Check if the array is complete */
+            if(!check_for_zeros(indexes[teams_counter]))
+                break;
+        }
+    }
+    /* 1.Print max. number of teams */
+    printf("%d\n", max_teams);
+
+    /* 2.Print the indexes of the students */
+    for(int teams_counter = 0; teams_counter < max_teams; ++teams_counter)
+        print_array(indexes[teams_counter], 3);
+}
+
 int main()
 {
     int number_of_students, counter = 0;
     scanf("%d" ,&number_of_students);
 
-    int *child = (int *) calloc(number_of_students, sizeof(int));
+    int *skills = (int *) calloc(number_of_students, sizeof(int));
     for(int i = 0; i < number_of_students; ++i)
-        scanf("%d", &child[i]);
+        scanf("%d", &skills[i]);
 
     /* Find the frequency of each skill */
     int freq[4] = {0, 0, 0, 0};
     for(int i = 0; i < number_of_students; ++i)
-        ++freq[child[i]];
+        ++freq[skills[i]];
 
     ASC_sort(freq, 4);
     int max_teams = freq[1];
@@ -55,44 +114,5 @@ int main()
         return 0;
     }
 
-    int indexes[max_teams][3];
-    for(int i = 0; i < max_teams; i++)
-        for(int j = 0; j < 3; j++)
-            indexes[i][j] = 0;
-
-
-
-    for(int teams_counter = 0; teams_counter < max_teams; ++teams_counter){
-        counter = 0;
-        for(int skills_counter = 0; skills_counter < number_of_students; ++skills_counter){
-            if (child[skills_counter] == 1){
-                if(indexes[teams_counter][0] == 0){
-                    indexes[teams_counter][0] = skills_counter + 1;
-                    child[skills_counter] = 0;
-                }
-            }
-            else if (child[skills_counter] == 2){
-                if(indexes[teams_counter][1] == 0){
-                    indexes[teams_counter][1] = skills_counter + 1;
-                    child[skills_counter] = 0;
-                }
-            }
-            else if (child[skills_counter] == 3){
-                if(indexes[teams_counter][2] == 0){
-                    indexes[teams_counter][2] = skills_counter + 1;
-                    child[skills_counter] = 0;
-                }
-            }
-            if(!check_if_zero(indexes[teams_counter]))
-                    break;
-        }
-    }
-    printf("%d\n", max_teams);
-
-    for(int teams_counter = 0; teams_counter < max_teams; ++teams_counter){
-        for(int skills_counter = 0; skills_counter < 3; ++skills_counter){
-            printf("%d ", indexes[teams_counter][skills_counter]);
-        }
-        printf("\n");
-    }
+    solve(skills, number_of_students, max_teams);    
 }

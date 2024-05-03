1174b64cef870bd1db9d592492fefe464f658271
Mostafa_Asaad <ma4190243@gmail.com>
2024-05-03 12:10:01 +0300

    Problem 26 on LeetCode

--- Leetcode/Remove_Duplicates_From_Sorted_Array.c
+++ Leetcode/Remove_Duplicates_From_Sorted_Array.c
@@ -10,19 +10,19 @@ int removeDuplicates(int* nums, int numsSize)
     if (numsSize == 0) 
         return 0;
 
-    unsigned int k = 0; 
+    unsigned int k = 1; 
     for (unsigned int i = 1; i < numsSize; ++i) 
     {
-        if (nums[i] != nums[k])
-            nums[++k] = nums[i];
+        if (nums[i] != nums[k - 1])
+            nums[k++] = nums[i];
     }
 
     // Number of unique elements is one more than the uniqueIndex
-    return k + 1;
+    return k;
 }
 int main()
 {
-    int nums[] = {1, 2, 2};
+    int nums[] = {1, 1, 1, 2, 3, 4, 4};
     int numberOfUniqueElements = removeDuplicates(nums, sizeof(nums) / sizeof(nums[0]));
 
     printf("numberOfUniqueElements = %d\n", numberOfUniqueElements);
